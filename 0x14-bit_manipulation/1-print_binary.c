91cf51321104b11d6b23e3c568ca5cba74b6f924
RachaelAjayi <yemak31@gmail.com>
2023-10-26 21:59:28 -0700

    B

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n < 2)
+	if (n >> 0)
 	{
-
-		_putchar(n + '0');
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 
 	else
 	{
-	print_binary(n >> 1);
+		_putchar('0');
 
 	}
 }
